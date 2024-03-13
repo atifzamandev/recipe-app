869522e14f5afc4762d3bd89644414d39bbc5b09
Atif Zaman <atifzaman88010@gmail.com>
2024-03-13 20:35:27 +0100

    Implemented Context API For Data And LocalStroage

--- src/api/recipes.ts
+++ src/api/recipes.ts
@@ -1,5 +1,5 @@
-import axios from 'axios'
 import { useQuery } from 'react-query'
+import axios from 'axios'
 import { RecipeProps } from '../types/recipe'
 
 interface ApiParamsProps {
@@ -17,9 +17,13 @@ const fetchRecipesData = async ({
   cuisine,
   to,
 }: ApiParamsProps): Promise<RecipeProps> => {
+  //As a good practice. These values should come from .env file but lets have them for test purpose.
+  const REACT_APP_API_BASE_URL = 'https://api.edamam.com/search'
+  const REACT_APP_API_ID = 'de45abea'
+  const REACT_APP_API_KEY = '3661e79c36c22ffac791a5bac5a31532'
   try {
     const { data } = await axios.get(
-      `${process.env.REACT_APP_API_BASE_URL}?&q=${query}&app_id=${process.env.REACT_APP_API_ID}&app_key=${process.env.REACT_APP_API_KEY}&from=0&to=${to}&health=${health}&diet=${diet}&cuisineType=${cuisine}`
+      `${REACT_APP_API_BASE_URL}?&q=${query}&app_id=${REACT_APP_API_ID}&app_key=${REACT_APP_API_KEY}&from=0&to=${to}&health=${health}&diet=${diet}&cuisineType=${cuisine}`
     )
 
     return data
